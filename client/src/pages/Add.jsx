ef5ec5293217ea0cd3746ab326e6ebc5257845ee
unknown <jgh7646@gmail.com>
2022-11-23 08:37:08 +0900

    error

---
+++ client/src/pages/Add.jsx
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Add = () => {
+  const [book, setBook] = useState({
+    title: '',
+    desc: '',
+    price: null,
+    cover: '',
+  });
+
+  const navigate = useNavigate();
+  //how to usestate properly
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    // console.log(book);///
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:8800/books', book);
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //////
+
+  return (
+    <div className='form'>
+      <h1>Add New BOok</h1>
+      <input
+        type='text'
+        placeholder='title'
+        onChange={handleChange}
+        name='title'
+      />
+      <input
+        type='text'
+        placeholder='desc'
+        onChange={handleChange}
+        name='desc'
+      />
+      <input
+        type='text'
+        placeholder='price'
+        onChange={handleChange}
+        name='price'
+      />
+      <input
+        type='text'
+        placeholder='cover'
+        onChange={handleChange}
+        name='cover'
+      />
+      <button onClick={handleClick}>Add books</button>
+    </div>
+  );
+};
+
+export default Add;
